d240bec39be91997656b832d01c4119c79a978a6
bunik82 <124196876+bunik82@users.noreply.github.com>
2023-02-14 17:16:44 +0100

    Homework2
    Ich habe erledigt

---
+++ homeWork.java
@@ -0,0 +1,23 @@
+package home;
+
+public class homeWork {
+
+    public static void main(String[] args){
+
+      int  n  = 365;
+
+      int e = n % 10;
+      int d = n/ 10 % 10;
+      int s = n/ 100;
+        System.out.println(e);
+        System.out.println(d);
+        System.out.println(s);
+
+
+
+
+
+
+    }
+
+}
